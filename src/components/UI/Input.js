8b3cf895aac8b6c986cedad91833ff7ea4dd546d
Jan Golder <kontakt.jangolder@gmail.com>
2023-02-12 19:22:38 +0100

    add cart update logic

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = props =>{
+const Input = React.forwardRef((props, ref) =>{
     return <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
         {/* {...props.input} - to specjalny zapis, który gwarantuje, że jeśli w obiekcie input będzie jakis atrybut, stanowiący cechę inputa np. type="text", to zostanie dodany do tego tagu  */}
          
-        <input {...props.input}/>
+        <input ref={ref} {...props.input}/>
     </div>
-};
+});
 
 export default Input;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,5 +1,32 @@
-import classes from './Modal.modules.css';
+import { Fragment } from "react";
+import ReactDom from "react-dom";
 
-const Modal = props =>{};
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}/>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDom.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {ReactDom.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
